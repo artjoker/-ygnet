b8500e6c7e69caafa42bacb86eb4e13348684ce8
Carsten Lorenz <carsten.lorenz@projectstage.org>
2016-09-19 13:18:13 +0200

    - enable Threads in destop

--- src/main/java/com/cygnet/ourdrive/gui/Desktop.java
+++ src/main/java/com/cygnet/ourdrive/gui/Desktop.java
@@ -20,7 +20,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 
-public class Desktop {
+public class Desktop extends Thread {
 
     private static final Logger logger = LoggerFactory.getLogger(OurDriveService.class);
 
